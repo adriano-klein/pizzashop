982920d5d124c4aca2cc2d8d1120afdb251c42ca
adriano25klein@gmail.com <you@example.com>
2024-10-23 07:24:19 -0300

    Pagina nao encontrada

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
+import { NotFound } from './pages/404'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/Sign-up'
@@ -10,6 +11,7 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [{ path: '/', element: <Dashboard /> }],
   },
   {
