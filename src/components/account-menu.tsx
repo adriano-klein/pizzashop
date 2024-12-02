495fcceaf36725d16395cacfe839b511d7693c13
adriano25klein@gmail.com <you@example.com>
2024-12-02 11:17:10 -0300

    criei o modal de edicao de perfil

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,10 +1,13 @@
+import { DialogTrigger } from '@radix-ui/react-dialog'
 import { useQuery } from '@tanstack/react-query'
 import { Building, ChevronDown, LogOut } from 'lucide-react'
 
 import { getManagedRestaurant } from '@/api/get-managed-restaurant'
 import { getProfile } from '@/api/get-profile'
 
+import { StoreProfileDialog } from './store-profile-dialog'
 import { Button } from './ui/button'
+import { Dialog } from './ui/dialog'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -13,49 +16,72 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from './ui/dropdown-menu'
+import { Skeleton } from './ui/skeleton'
 
 export function AccountMenu() {
-  const { data: profile } = useQuery({
+  // NOTE: usando o ReactQuery nos ajuda a saber se a requisição está em andamento ou não através do isLoading e usar, por exemplo, o skeleton
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
+    // NOTE: A queryKey serve para identificar unicamente uma requisição na aplicação para evitar que a mesma seja feita mais de uma vez
     queryKey: ['profile'],
     queryFn: getProfile,
   })
 
-  const { data: managedRestaurant } = useQuery({
+  const { data: managedRestaurant, isLoading: isLoadingRestaurant } = useQuery({
     queryKey: ['managed-restaurant'],
     queryFn: getManagedRestaurant,
   })
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant={'outline'}
-          className="flex select-none items-center gap-2"
-        >
-          {managedRestaurant?.name}
-          <ChevronDown className="h-4 w-4" />
-        </Button>
-      </DropdownMenuTrigger>
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant={'outline'}
+            className="flex select-none items-center gap-2"
+          >
+            {/* NOTE: se estiver carregando o skeleton é mostrado, caso contrário é
+          mostrado o nome do restaurante */}
+            {isLoadingRestaurant ? (
+              <Skeleton className="h-4 w-40" />
+            ) : (
+              managedRestaurant?.name
+            )}
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
 
-      <DropdownMenuContent align="end" className="w-56">
-        <DropdownMenuLabel className="flex flex-col">
-          <span> {profile?.name} </span>
-          <span className="text-xs font-normal text-muted-foreground">
-            {profile?.email}
-          </span>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
+        <DropdownMenuContent align="end" className="w-56">
+          <DropdownMenuLabel className="flex flex-col">
+            {isLoadingProfile ? (
+              <div className="space-y-1.5">
+                <Skeleton className="h-4 w-32" />
+                <Skeleton className="h-3 w-24" />
+              </div>
+            ) : (
+              <>
+                <span> {profile?.name} </span>
+                <span className="text-xs font-normal text-muted-foreground">
+                  {profile?.email}
+                </span>
+              </>
+            )}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
 
-        <DropdownMenuItem>
-          <Building className="mr-2 h-4 w-4" />
-          <span>Perfil da loja</span>
-        </DropdownMenuItem>
+          <DialogTrigger asChild>
+            <DropdownMenuItem>
+              <Building className="mr-2 h-4 w-4" />
+              <span>Perfil da loja</span>
+            </DropdownMenuItem>
+          </DialogTrigger>
+          <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+            <LogOut className="mr-2 h-4 w-4" />
+            <span>Sair</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
 
-        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-          <LogOut className="mr-2 h-4 w-4" />
-          <span>Sair</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      <StoreProfileDialog />
+    </Dialog>
   )
 }
---
+++ src/components/store-profile-dialog.tsx
@@ -0,0 +1,83 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { DialogDescription, DialogTitle } from '@radix-ui/react-dialog'
+import { useQuery } from '@tanstack/react-query'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { getManagedRestaurant } from '@/api/get-managed-restaurant'
+
+import { Button } from './ui/button'
+import { DialogContent, DialogFooter, DialogHeader } from './ui/dialog'
+import { Input } from './ui/input'
+import { Label } from './ui/label'
+import { Textarea } from './ui/textarea'
+
+const storeProfileSchema = z.object({
+  name: z.string().min(1),
+  description: z.string(),
+})
+
+type StoreProfileSchema = z.infer<typeof storeProfileSchema>
+
+export function StoreProfileDialog() {
+  const { data: managedRestaurant } = useQuery({
+    // NOTE: esta requisição foi feita no account-menu e não está sendo feita novamente aqui, por conta do querykey ser o mesmo
+    queryKey: ['managed-restaurant'],
+    queryFn: getManagedRestaurant,
+  })
+
+  const { register, handleSubmit } = useForm<StoreProfileSchema>({
+    resolver: zodResolver(storeProfileSchema),
+
+    values: {
+      name: managedRestaurant?.name ?? '',
+      description: managedRestaurant?.description ?? '',
+    },
+  })
+
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Perfil do loja</DialogTitle>
+        <DialogDescription>
+          {' '}
+          Atualize as informações do seu estabelecimento visíveis ao cliente
+        </DialogDescription>
+      </DialogHeader>
+
+      <form>
+        <div className="space-y-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="name">
+              Nome
+            </Label>
+            <Input
+              className="col-span-3"
+              id="name"
+              {...register('name')}
+            ></Input>
+          </div>
+
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="description">
+              Descrição
+            </Label>
+            <Textarea
+              className="col-span-3"
+              id="description"
+              {...register('description')}
+            ></Textarea>
+          </div>
+        </div>
+        <DialogFooter>
+          <Button variant="ghost" type="button">
+            Cancelar
+          </Button>
+          <Button variant="success" type="submit">
+            Salvar
+          </Button>
+        </DialogFooter>
+      </form>
+    </DialogContent>
+  )
+}
