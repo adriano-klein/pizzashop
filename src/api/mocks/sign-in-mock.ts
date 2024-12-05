fc35967693edfdfa0477069b8ae8f6be5964d13d
adriano25klein@gmail.com <you@example.com>
2024-12-05 17:03:41 -0300

    configurei o MSW em SignIn

---
+++ src/api/mocks/sign-in-mock.ts
@@ -0,0 +1,19 @@
+import { http, HttpResponse } from 'msw'
+
+import { SignInBody } from '../sign-in'
+export const signInMock = http.post<never, SignInBody>(
+  '/authenticate',
+  async ({ request }) => {
+    const { email } = await request.json()
+
+    if (email === 'adriano25klein@gmail.com') {
+      return new HttpResponse(null, {
+        status: 200,
+        headers: {
+          'set-cookie': 'auth=sample-jwt',
+        },
+      })
+    }
+    return new HttpResponse(null, { status: 401 })
+  },
+)
