c3b23ca44ccb5025c065a083ca4baf375ea0babf
adriano25klein@gmail.com <you@example.com>
2024-12-05 09:49:22 -0300

    Finalizei os outros testes em pagination

--- src/components/pagination.spec.tsx
+++ src/components/pagination.spec.tsx
@@ -9,14 +9,22 @@ const onPageChangeCallBack = vi.fn()
 // se uma função foi chamada, quantas vezes foi chamada, com quais argumentos foi
 // chamada, e outras informações relevantes. Spies são particularmente úteis para
 // testar funções de callback, métodos de API, e outras interações que ocorrem dentro do código.
+
 describe('Pagination', () => {
+  // NOTE:  O trecho de código beforeEach(() => { onPageChangeCallBack.mockClear() })
+  // tem a função de limpar o estado do mock onPageChangeCallBack antes de cada teste.
+  // Isso garante que cada teste comece com um estado limpo e não seja influenciado por
+  // chamadas de função ou estados de testes anteriores.
+  beforeEach(() => {
+    onPageChangeCallBack.mockClear()
+  })
   it('should display the right amount of pages and results', () => {
     const wrapper = render(
       <Pagination
         pageIndex={0}
         totalCount={200}
         perPage={10}
-        onPageChange={() => {}}
+        onPageChange={onPageChangeCallBack}
       />,
     )
 
@@ -44,5 +52,68 @@ describe('Pagination', () => {
     await user.click(nextPageButton)
 
     expect(onPageChangeCallBack).toHaveBeenCalledWith(1)
+  })
+
+  it('should be able to navigate to the previous page', async () => {
+    const wrapper = render(
+      <Pagination
+        pageIndex={1}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallBack}
+      />,
+    )
+
+    const previousPageButton = wrapper.getByRole('button', {
+      // NOTE: este é o conteúdo do botão
+      name: 'Página anterior',
+    })
+
+    const user = userEvent.setup()
+    await user.click(previousPageButton)
+
+    expect(onPageChangeCallBack).toHaveBeenCalledWith(0)
+  })
+
+  it('should be able to navigate to the first page', async () => {
+    const wrapper = render(
+      <Pagination
+        pageIndex={5}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallBack}
+      />,
+    )
+
+    const firstPageButton = wrapper.getByRole('button', {
+      // NOTE: este é o conteúdo do botão
+      name: 'Primeira página',
+    })
+
+    const user = userEvent.setup()
+    await user.click(firstPageButton)
+
+    expect(onPageChangeCallBack).toHaveBeenCalledWith(0)
+  })
+
+  it('should be able to navigate to the last page', async () => {
+    const wrapper = render(
+      <Pagination
+        pageIndex={5}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallBack}
+      />,
+    )
+
+    const lastPageButton = wrapper.getByRole('button', {
+      // NOTE: este é o conteúdo do botão
+      name: 'Última página',
+    })
+
+    const user = userEvent.setup()
+    await user.click(lastPageButton)
+
+    expect(onPageChangeCallBack).toHaveBeenCalledWith(19)
   })
 })
