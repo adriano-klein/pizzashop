1e0e699c60064123b5e19f8be5089abbfa05d182
adriano25klein@gmail.com <you@example.com>
2024-12-17 10:42:44 -0300

    Teste E@E: Sign up

---
+++ test/sign-up.e2e-spec.ts
@@ -0,0 +1,31 @@
+import { expect, test } from '@playwright/test'
+
+test('sign up successfully', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+  await page.getByLabel('Nome do estabelecimento').fill('Pizza Shop')
+  await page.getByLabel('Seu Nome').fill('Adriano Klein')
+  await page.getByLabel('Seu E-mail').fill('adriano25klein@gmail.com')
+  await page.getByLabel('Seu celular').fill('11999999999')
+  await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
+  const toast = page.getByText('Restaurante cadastrado com sucesso')
+
+  expect(toast).toBeVisible()
+})
+
+test('sign up with error', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+  await page.getByLabel('Nome do estabelecimento').fill('Pizzaria do fulano')
+  await page.getByLabel('Seu Nome').fill('Fulano')
+  await page.getByLabel('Seu E-mail').fill('fulano25klein@gmail.com')
+  await page.getByLabel('Seu celular').fill('11999999999')
+  await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
+  const toast = page.getByText('Erro ao cadastrar restaurante')
+
+  expect(toast).toBeVisible()
+})
+
+test('navigate to login page', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+  await page.getByRole('link', { name: 'Fazer login' }).click()
+  expect(page.url()).toContain('/sign-in')
+})
